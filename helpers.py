f86fb2217ca5f98b8b4116f3accf4025cf1047d4
Alex King <alex@likening.co.uk>
2023-12-13 15:40:40 +0000

    update

--- helpers.py
+++ helpers.py
@@ -1,5 +1,4 @@
 
-
 def base_convert(num, base) -> list[int]:
     """
     Converts num to base number.
@@ -14,4 +13,28 @@ def base_convert(num, base) -> list[int]:
     return _result
 
 
+def get_column(arr, idx, col) -> str:
+    """
+    Constructs column of 2D array.
+    :param arr: 2D array
+    :param idx: 1st index of 2D array
+    :param col: Column to construct
+    :return:
+    """
+    if col < 0:
+        raise IndexError
 
+    return "".join([bl[col] for bl in arr[idx]])
+
+
+def find_differences(str1, str2) -> int:
+    """
+    Find number of differences between two strings
+    :param str1: String 1
+    :param str2: String 2
+    :return:
+    """
+    if len(str1) != len(str2):
+        raise IndexError
+
+    return sum([1 if str1[d] != str2[d] else 0 for d in range(len(str1))])
